d74a3f5416923019eac89bae4530e2eeced77b1a
Er1tain <sanyapridava@mail.ru>
2024-09-28 18:46:45 +0300

    feat: given count rows/columns with useLocalStorage

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,12 @@
+import useLocalStorage from "../app/hooks/useLocalStorage";
+
 export default function Game() {
+    //Количество строк/столбцов(их кол-во одинаково)
+    const frame_size = Number(useLocalStorage('frame_size')[0]);
+
     return (
             <div>
-
+                <h1>Игра</h1>
             </div>
     );
 }
