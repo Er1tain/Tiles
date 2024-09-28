7e295fa93b8c3b022085c0e0ef979efbc1ad95bd
Er1tain <sanyapridava@mail.ru>
2024-09-28 19:15:04 +0300

    feat: added back to settings

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from "react-router";
 import useLocalStorage from "../app/hooks/useLocalStorage";
 import GameFrame from "../widgets/GameFrame";
 
 export default function Game() {
     //Количество строк/столбцов(их кол-во одинаково)
     const frame_size = Number(useLocalStorage('frame_size')[0]);
+    const nav = useNavigate();
+    const goToSettings = ()=>nav('/');
 
     return (
             <div>
                 <h1>Игра</h1>
                 <GameFrame size={frame_size}/>
+                <button onClick={goToSettings}>К настройкам</button>
             </div>
     );
 }
