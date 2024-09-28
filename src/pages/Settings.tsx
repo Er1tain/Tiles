f5a1105e32290a903aceb5b57415e649a9284a1e
Er1tain <sanyapridava@mail.ru>
2024-09-28 15:27:14 +0300

    feat: create pages of Settings and Game

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+export default function Settings() {
+    return (
+        <div>
+            <h1>Размер поля: </h1>
+            <select>
+                <option>2x2</option>
+                <option>4x4</option>
+                <option>8x8</option>
+            </select>
+        </div>
+    );
+}
