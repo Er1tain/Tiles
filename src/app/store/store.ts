1f0930f411254d81c802fb33eedc189c00ae3a8c
Er1tain <sanyapridava@mail.ru>
2024-09-28 15:26:40 +0300

    feat: create store

---
+++ src/app/store/store.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface Move {
+    1: string,
+    2: string
+}
+
+export type FrameSize = "2x2"|"4x4"|"8x8";
+
+const initialStateMoves: Move[] = []; 
+
+const gameSlice = createSlice({
+    name: "game",
+    initialState: {
+        frame_size: "",
+        moves: initialStateMoves
+    },
+
+    reducers: {
+        choiceSize(state, action: PayloadAction<FrameSize>) {
+            state.frame_size = action.payload;
+        },
+
+        addMove(state, action: PayloadAction<Move>) {
+            state.moves.push(action.payload);
+        }
+    }
+
+});
+
+export const {choiceSize, addMove} = gameSlice.actions;
+export default gameSlice.reducer;
