041babf7a3ec2459d221d7e7a2f44cd901e0e85f
Er1tain <sanyapridava@mail.ru>
2024-09-28 15:41:09 +0300

    feat: decorated pages in Provider redux

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,12 @@
+import { Provider } from "react-redux";
+import { store } from "../app/store/store";
+
 export default function Game() {
     return (
-        <div>
-            
-        </div>
+        <Provider store={store}>
+            <div>
+
+            </div>
+        </Provider>
     );
 }
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,12 +1,17 @@
+import { Provider } from "react-redux"
+import { store } from "../app/store/store";
+
 export default function Settings() {
     return (
-        <div>
-            <h1>Размер поля: </h1>
-            <select>
-                <option>2x2</option>
-                <option>4x4</option>
-                <option>8x8</option>
-            </select>
-        </div>
+        <Provider store={store}>
+            <div>
+                <h1>Размер поля: </h1>
+                <select>
+                    <option>2x2</option>
+                    <option>4x4</option>
+                    <option>8x8</option>
+                </select>
+            </div>
+        </Provider>
     );
 }
