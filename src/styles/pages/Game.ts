28bc7e737c67b6c90355dd750a4bb3bf72c8b653
Er1tain <sanyapridava@mail.ru>
2024-10-01 02:02:22 +0300

    feat: begin styling game frame

--- src/styles/pages/Game.ts
+++ src/styles/pages/Game.ts
@@ -8,5 +8,16 @@ export const Container = styled.div`
 
 export const SettingsButton = styled.button`
     width: max-content;
-    margin-top: 10px;
+    margin-top: 50px;
+`
+export const RowTiles = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+`
+
+export const ColsTiles = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 `
--- src/widgets/GameFrame/GameFrame.tsx
+++ src/widgets/GameFrame/GameFrame.tsx
@@ -6,6 +6,7 @@ import { shuffle } from "./logic";
 import useStoreForSendMove from "./useStoreForSendMove";
 import MessageAboutNewRound from "../../app/modal_windows/MessageAboutNewRound";
 import TilesContextProvider from "../../app/hooks/useTilesContext";
+import { ColsTiles, RowTiles } from "../../styles/pages/Game";
 
 export const initStateChoiceTiles: ChoiceTilesType = {
     first_tiles: {
@@ -30,25 +31,25 @@ export default function GameFrame({size}: IGameFrame) {
         const list_colors_for_row_tiles = colors_for_tiles.slice(index_start, index_start + size);
 
         return (
-            <div>
+            <RowTiles>
                 {list_colors_for_row_tiles.map((color, i)=>{
                     if (i == size - 1) index_start += size;
                     return <Tiles num_row={num_row.toString()} num_col={i.toString()} backgroundColor={color} choice_tiles={choice_tiles} setChoiceTiles={setChoiceTiles}/>
                 })}
-            </div>
+            </RowTiles>
         );
     }
 
     const generateColumns = ()=>{
         return (
             <>
-            <div style={{position: "absolute"}}>
+            <ColsTiles style={{position: "absolute"}}>
                 {new Array(size).fill('').map((_, i)=>{
                     return (
                         <div>{generateRowTiles(i)}</div>
                     )
                 })}
-            </div>
+            </ColsTiles>
             <MessageAboutNewRound text="Раунд: " color_text="red" choice_tiles={choice_tiles}/>
             </>
         );
