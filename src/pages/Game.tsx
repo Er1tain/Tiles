e9ccc9330e9a7a10625b15db16e863d5207b3f90
Er1tain <sanyapridava@mail.ru>
2024-09-30 23:23:23 +0300

    feat: default set frame if user doesnt choose

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -4,15 +4,16 @@ import GameFrame from "../widgets/GameFrame/GameFrame";
 import { Container, SettingsButton } from "../styles/pages/Game";
 
 export default function Game() {
-    //Количество строк/столбцов(их кол-во одинаково)
-    const frame_size = Number(useLocalStorage('frame_size')[0]);
     const nav = useNavigate();
     const goToSettings = ()=>nav('/');
 
+    //Количество строк/столбцов(их кол-во одинаково)
+    const frame_size: number|null = Number(useLocalStorage('frame_size')?.[0]);;
+    
     return (
             <Container>
                 <h1>Игра</h1>
-                <GameFrame size={frame_size}/>
+                <GameFrame size={frame_size ? frame_size : 2}/>
                 <SettingsButton onClick={goToSettings}>К настройкам</SettingsButton>
             </Container>
     );
