75b32f607a316963c4c87839892dd3d84865a68a
Er1tain <sanyapridava@mail.ru>
2024-10-01 01:02:07 +0300

    feat: redirect to result if game finished

--- src/widgets/GameFrame/blockedTiles.ts
+++ src/widgets/GameFrame/blockedTiles.ts
@@ -1,13 +1,12 @@
 import { ChoiceTilesType } from "./types"
 import { COLOR_TRUE_MOVE } from "../../shared/COLORS";
 
-let count_click = 0;
-
-export default function blockedTiles(choice_tiles: ChoiceTilesType, frame_size: number) {
+export default function blockedTiles(choice_tiles: ChoiceTilesType) {
 
     document.querySelectorAll("button").forEach(tile=>{
-        if (tile.value == choice_tiles.first_tiles.coords || tile.value == choice_tiles.second_tiles.coords) tile.style.background = COLOR_TRUE_MOVE;
+        if (tile.value == choice_tiles.first_tiles.coords || tile.value == choice_tiles.second_tiles.coords) {
+            tile.style.background = COLOR_TRUE_MOVE;
+        }
     });
-    count_click += 2;
-    if (count_click == frame_size * 2) window.location.href = '/result';
+    
 }
--- src/widgets/GameFrame/useStoreForSendMove.ts
+++ src/widgets/GameFrame/useStoreForSendMove.ts
@@ -1,15 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { ChoiceTilesType } from "./types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { addMove, beginNewRound, FrameSize, Move, TypeState } from "../../app/store/store";
 import { initStateChoiceTiles } from "./GameFrame";
 import blockedTiles from "./blockedTiles";
+import { useNavigate } from "react-router";
 
 export default function useStoreForSendMove([choice_tiles, setChoiceTiles]: [ChoiceTilesType, React.Dispatch<React.SetStateAction<ChoiceTilesType>>]) {
     const size = Number((useSelector<TypeState>(state => state.frame_size) as FrameSize)[0]);
 
+    const [countSameClick, setCountSameClick] = useState<number>(0);
+
     const dispatch = useDispatch();
+    const nav = useNavigate();
     useEffect(()=>{
+        if (countSameClick == size * size) nav('/result');
         if (choice_tiles?.first_tiles.color_tiles != "" && choice_tiles?.second_tiles.color_tiles != "") {
             
             //Отправляем в store ход в соответствии с типом данных Move
@@ -22,7 +27,13 @@ export default function useStoreForSendMove([choice_tiles, setChoiceTiles]: [Cho
 
             if (choice_tiles?.second_tiles.color_tiles != "") setChoiceTiles(initStateChoiceTiles);
 
-            choice_tiles.first_tiles.color_tiles == choice_tiles.second_tiles.color_tiles ? blockedTiles(choice_tiles, size) : dispatch(beginNewRound());
+            if (choice_tiles.first_tiles.color_tiles == choice_tiles.second_tiles.color_tiles) {
+                blockedTiles(choice_tiles);
+
+                let current_count = countSameClick;
+                current_count += 2;
+                setCountSameClick(current_count);
+            } else dispatch(beginNewRound());
         }
-    }, [choice_tiles, dispatch, setChoiceTiles])
+    }, [choice_tiles, dispatch, setChoiceTiles, countSameClick, setCountSameClick])
 }
